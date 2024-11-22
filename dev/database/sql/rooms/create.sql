006004edb8c8c10c03ded2387fcde5bcabbd2b6d
yukihito-jokyu <ok220020@oks.iput.ac.jp>
2024-11-22 21:44:43 +0900

    roomsテーブルとchatsテーブルの再定義

--- dev/database/sql/rooms/create.sql
+++ dev/database/sql/rooms/create.sql
@@ -1,7 +1,10 @@
-CREATE TABLE hist_room (
+CREATE TABLE rooms (
     room_id VARCHAR(255) PRIMARY KEY,
     csv_id VARCHAR(255) NOT NULL,
+    visualization_type VARCHAR(255) NOT NULL,
     vertical VARCHAR(255) NOT NULL,
+    horizontal VARCHAR(255) NOT NULL,
     target VARCHAR(255) NOT NULL,
+    regression BOOLEAN NOT NULL,
     FOREIGN KEY (csv_id) REFERENCES csvs(csv_id)
 )
--- dev/database/sql/rooms/pie.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE pie_room (
-    room_id VARCHAR(255) PRIMARY KEY,
-    csv_id VARCHAR(255) NOT NULL,
-    vertical VARCHAR(255) NOT NULL,
-    horizontal VARCHAR(255) NOT NULL,
-    FOREIGN KEY (csv_id) REFERENCES csvs(csv_id)
-)
